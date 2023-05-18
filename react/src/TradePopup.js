631aa7d52de97a48be697a52bef93f4e30295393
maxstc <a@a.a>
2023-05-18 16:14:19 -0400

    made popup display countries dynamically

--- react/src/TradePopup.js
+++ react/src/TradePopup.js
@@ -5,33 +5,32 @@ import Clickable from "./Clickable";
 
 const TradePopup = (props) => {
 
+    const [hidden, setHidden] = useState(true);
     const [selectTarget, setSelectTarget] = useState(0);
     const [selectUser, setSelectUser] = useState(0);
 
-    const [hover, setHover] = useState(-1);
-    const [click, setClick] = useState(-1);
+    const hiddenStyle = {
+        display: hidden ? "none" : "initial"
+    }
 
-    function isSelected(id, isTarget) {
-        if (isTarget) {
-            return (selectTarget === id) ? "selected" : "unselected";
-        }
-        else {
-            return (selectUser === id) ? "selected" : "unselected";
-        }
+    function sendProposal(a, b, c) {
+        console.log(a);
+        console.log(b);
+        console.log(c);
     }
 
     return (
-        <div className="popupBackground">
+        <div className="popupBackground" style={hiddenStyle}>
             <div className="popup">
                 <p className="centerText">Trade Offer</p>
                 <div className="tradeColumn">
                     <p className="centerText">Their Countries</p>
-                    <Clickable clickCallback={()=>{}} inside={
+                    <Clickable clickCallback={()=>{setSelectTarget(0)}} inside={
                         <p className="centerText">
                             {props.targetTeam.countries[0].flag}
                         </p>
                     }/>
-                    <Clickable clickCallback={()=>{}} inside={
+                    <Clickable clickCallback={()=>{setSelectTarget(1)}} inside={
                         <p className="centerText">
                             {props.targetTeam.countries[1].flag}
                         </p>
@@ -39,12 +38,12 @@ const TradePopup = (props) => {
                 </div>
                 <div className="tradeColumn">
                     <p className="centerText">Your Countries</p>
-                    <Clickable clickCallback={()=>{}} inside={
+                    <Clickable clickCallback={()=>{setSelectUser(0)}} inside={
                         <p className="centerText">
                             {props.userTeam.countries[0].flag}
                         </p>
                     }/>
-                    <Clickable clickCallback={()=>{}} inside={
+                    <Clickable clickCallback={()=>{setSelectUser(1)}} inside={
                         <p className="centerText">
                             {props.userTeam.countries[1].flag}
                         </p>
@@ -52,12 +51,12 @@ const TradePopup = (props) => {
                 </div>
                 <p className="centerText">Their {props.targetTeam.countries[selectTarget].flag} for your {props.userTeam.countries[selectUser].flag}</p>
                 <div className="tradeColumn">
-                    <Clickable clickCallback={()=>{}} inside={
+                    <Clickable clickCallback={()=>{sendProposal(props.userTeam, selectTarget, selectUser);setHidden(true)}} inside={
                         <p className="centerText">Confirm</p>
                     }/>
                 </div>
                 <div className="tradeColumn">
-                    <Clickable clickCallback={()=>{}} inside={
+                    <Clickable clickCallback={()=>{setHidden(true)}} inside={
                         <p className="centerText">Cancel</p>
                     }/>
                 </div>
