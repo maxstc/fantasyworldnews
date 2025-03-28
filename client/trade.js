f17de4a8d4678a8d842bec5a491c44c041ef106f
max <ajkdh.macks@gmail.com>
2025-03-28 16:50:56 -0700

    added trade window

--- client/trade.js
+++ client/trade.js
@@ -1,3 +1,11 @@
+let tradeWindow = `
+<p>Trade for COUTNRY<p>
+<p>Select a country to give up</p>
+<input type="radio" id="trade1" name="test" value="test2">
+<label for="trade1">Trade1</label>
+`;
+
 function openTradeWindow(countryCode) {
     alert("Trade for " + countryCode);
+    document.getElementById("trade").innerHTML = tradeWindow;
 }
