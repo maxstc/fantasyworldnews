ef8159de9a7e588c8c72a3f3c146b14508034506
maxstc <99021310+maxstc@users.noreply.github.com>
2026-08-30 17:44:49 -0700

    made controllers look in same folder

--- server/routes/account/_controller.js
+++ server/routes/account/_controller.js
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
-import { signup } from "../signup.js";
-import { login } from "../login.js";
+import { signup } from "./signup.js";
+import { login } from "./login.js";
 
 router.post("/signup", signup);
 router.post("/login", login);
--- server/routes/trade/_controller.js
+++ server/routes/trade/_controller.js
@@ -1,8 +1,8 @@
 import express from "express";
 const router = express.Router();
-import { propose } from "../propose.js";
-import { accept } from "../accept.js";
-import { decline } from "../decline.js";
+import { propose } from "./propose.js";
+import { accept } from "./accept.js";
+import { decline } from "./decline.js";
 
 router.post("/propose", propose);
 router.post("/accept", accept);
