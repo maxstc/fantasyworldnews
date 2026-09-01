0a0e2ba55f8416650d76304be4437529632ec41e
maxstc <99021310+maxstc@users.noreply.github.com>
2026-09-01 01:16:02 -0700

    simple version of dashboard/trades

--- server/routes/dashboard/trades.js
+++ server/routes/dashboard/trades.js
@@ -1,3 +1,36 @@
-export async function trades(req, res) {
-    res.status(200).json({ success: true });
+//Written by Max & Evan
+
+import pool from "../../db.js";
+import jwt from "jsonwebtoken";
+
+export async function trades (req, res) {
+    try {
+        const tradesQuery = await pool.query(
+            "SELECT * FROM trades WHERE game_id = $1", 
+            [req.body.gameID]
+        );
+
+        tradesQuery.rowCount;
+        tradesQuery.rows;
+        console.log(tradesQuery.rows);
+
+        // const output = {
+        //     trades: [
+        //         {
+        //             timestamp:
+        //             proposerTeam:
+        //             targetTeam:
+        //             proposerCountry:
+        //             targetCountry:
+        //         },
+        //     ]
+        // }
+    }
+    catch (error) {
+        //Some error happened
+        console.error(error);
+        return res.status(400).json({
+            message: "Uncaught JS Error."
+        });
+    }
 };
--- testing.js
+++ testing.js
@@ -1,12 +1,17 @@
-fetch("http://localhost:41399/api/account/signup", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify({
-    username: "waxy",
-    password: "fart1234567890",
-    email: "ajkdh.macks@gmail.com"
-  })
-})
-  .then(response => response.json());
+async function test() {
+    const fetchResult = await fetch("http://localhost:41399/api/account/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + "this is where my token would go... IF I HAD ONE!!!"
+        },
+        body: JSON.stringify({
+            username: "waxy",
+            password: "fart1234567890"
+        })
+    })
+    const jsonResult = await fetchResult.json();
+    console.log(jsonResult);
+}
+
+test();
