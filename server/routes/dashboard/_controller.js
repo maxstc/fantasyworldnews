7752ea11831a71b7e9c66d130bb9b4736c4d22d6
maxstc <99021310+maxstc@users.noreply.github.com>
2026-09-01 14:40:44 -0700

    progress on dashboard/trades api

--- server/routes/dashboard/_controller.js
+++ server/routes/dashboard/_controller.js
@@ -3,9 +3,15 @@ const router = express.Router();
 import { headlines } from "./headlines.js";
 import { trades } from "./trades.js";
 import { players } from "./players.js";
+import { checkParamsNotUndefined } from "#root/server/middleware/checkParamsNotUndefined.js";
+import { checkAuth } from "#/root/server/middleware/checkAuth.js";
 
 router.post("/headlines", headlines);
-router.post("/trades", trades);
+router.post(
+    "/trades",
+    checkParamsNotUndefined("gameID"),
+    checkAuth,
+    trades);
 router.post("/players", players);
 
 export { router };
--- server/routes/dashboard/trades.js
+++ server/routes/dashboard/trades.js
@@ -1,17 +1,36 @@
 //Written by Max & Evan
 
 import pool from "../../db.js";
-import jwt from "jsonwebtoken";
 
 export async function trades (req, res) {
     try {
+        //Check if this account is in this game
+        //Get their account ID first
+        const accountsQuery = await pool.query(
+            "SELECT * FROM accounts WHERE username = $1",
+            [req.user.username]
+        )
+        if (accountsQuery.rowCount < 1) {
+            return res.status(400).json({
+                message: "Logged in account no longer exists"
+            });
+        }
+        const accountID = accountsQuery.rows[0].account_id;
+        //check that that account is in the game were getting the trades for
+        const playersQuery = await pool.query(
+            "SELECT * FROM players WHERE account_id = $1 AND game_id = $2",
+            [accountID, req.body.gameID]
+        )
+        if (playersQuery.rowCount < 1) {
+            return res.status(400).json({
+                message: "Logged in account not in specified game"
+            });
+        }
         const tradesQuery = await pool.query(
             "SELECT * FROM trades WHERE game_id = $1", 
             [req.body.gameID]
         );
 
-        tradesQuery.rowCount;
-        tradesQuery.rows;
         console.log(tradesQuery.rows);
 
         // const output = {
