fb387d48735fe99e850f2937f5acf4c3bb1cd766
maxstc <99021310+maxstc@users.noreply.github.com>
2026-09-04 03:08:06 -0700

    add try catch to checkparams

--- server/middleware/checkParamsNotUndefined.js
+++ server/middleware/checkParamsNotUndefined.js
@@ -1,12 +1,18 @@
 export function checkParamsNotUndefined(paramNames) {
     return (req, res, next) => {
-        for (const paramName of paramNames) {
-            if (!req.body[paramName]) {
-                return res.status(400).json({
-                    message: `Missing parameter: ${paramName}`
-                });
+        try {
+            for (const paramName of paramNames) {
+                if (!req.body[paramName]) {
+                    return res.status(400).json({
+                        message: `Missing parameter: ${paramName}`
+                    });
+                }
             }
+            next();
         }
-        next();
+        catch (error) {
+            console.error();
+            res.status(400).json({ message: "Parameter error" });
+        }
     }
 }
