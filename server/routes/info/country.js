ff0a1819d4df8bd1ee27f92de5c8eed1fc030c05
ebooth03 <evanboothsoccertag@gmail.com>
2026-09-04 01:35:20 -0700

    country.js no longer has console.log() calls

--- server/routes/info/country.js
+++ server/routes/info/country.js
@@ -6,9 +6,7 @@ export async function country(req, res) {
     const gameID = req.body.gameID;
     const countryCode = req.body.countryCode;
     
-    console.log("Request body:", req.body);
-    console.log("Game ID:", req.body.gameID);
-    console.log("Country Code:", req.body.countryCode);
+
     try {
         const playerCheck = await pool.query(
             `
