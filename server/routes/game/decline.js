4bc9026a380586dbc5f034db465a4cf6f3995000
maxstc <99021310+maxstc@users.noreply.github.com>
2026-09-03 23:12:04 -0700

    untested decline functionality

--- server/routes/game/decline.js
+++ server/routes/game/decline.js
@@ -1,3 +1,8 @@
+//Written by Max with guidance from the Dalai Lama and Mr. House from FNV
+import pool from "#root/server/db.js";
+
 export async function decline (req, res) {
+    await pool.query("UPDATE game_invites SET status='declined' WHERE id = $1 AND (recipient_account_id = $2 OR sender_account_id = $3) ", [req.body.inviteID, req.user.id, req.user.id]);
+
     res.status(200).json({ success: true });
 };
