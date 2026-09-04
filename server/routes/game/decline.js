68cbd980eba04b2ca09aa13df4f3e2b040221332
maxstc <99021310+maxstc@users.noreply.github.com>
2026-09-04 03:35:17 -0700

    add try catch to decline

--- server/routes/game/decline.js
+++ server/routes/game/decline.js
@@ -2,7 +2,23 @@
 import pool from "#root/server/db.js";
 
 export async function decline (req, res) {
-    await pool.query("UPDATE game_invites SET status='declined' WHERE id = $1 AND (recipient_account_id = $2 OR sender_account_id = $3) ", [req.body.inviteID, req.user.id, req.user.id]);
-
-    res.status(200).json({ success: true });
+    try {
+        const declineQuery = await pool.query(
+            `
+            UPDATE game_invites 
+            SET status='declined' 
+            WHERE STATUS = 'pending' 
+            AND id = $1 
+            AND (recipient_account_id = $2 OR sender_account_id = $2)
+            RETURNING *;
+            `,
+            [req.body.inviteID, req.user.id]);
+        if (declineQuery.rowCount > 0) {
+            res.status(200).json({});
+        }
+        res.status(400).json({ message: "Invite not found" });
+    }
+    catch (error) {
+        res.status(400).json({ message: "Decline failed" });
+    }
 };
