8e381d51f91d00db91809ea69ef61af74341487b
maxstc <99021310+maxstc@users.noreply.github.com>
2026-09-07 04:52:25 -0700

    add untested getinvites

--- server/routes/game/getInvites.js
+++ server/routes/game/getInvites.js
@@ -1,3 +1,13 @@
+//Written by Max with guidance from the Dalai Lama and Mr. House from FNV
+import pool from "#root/server/db.js";
+
 export async function getInvites (req, res) {
-    res.status(200).json({ success: true });
+    try {
+        const inviteQuery = await pool.query("SELECT * FROM game_invites' WHERE recipient_id = $1 OR sender_id = $1;",
+            [req.body.inviteID, req.user.id]);
+        res.status(200).json({invites: inviteQuery.rows});
+    }
+    catch (error) {
+        res.status(400).json({ message: "Get invites failed" });
+    }
 };
